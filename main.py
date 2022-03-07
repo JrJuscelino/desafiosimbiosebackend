be344f35cd6c5cd02d79d94883d50a3414530bbc
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-03-07 20:25:33 -0300

    WORKS

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
 
 app = Flask(__name__)
 
+engine = create_engine('mysql+pymysql://root:password@localhost/test', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+Base.metadata.create_all(engine)
+
+
+# try write a good documentation
 @app.route('/teams', methods=['POST'])
 def register_teams():
     # Get data from the POST body
